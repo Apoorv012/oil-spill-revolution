797b578b79337009aef320327b1ad5ceaa60601c
Apoorv012 <apoorvmitthu@gmail.com>
2022-07-24 00:34:28 +0530

    removed the iss satelite part

--- app.js
+++ app.js
@@ -21,40 +21,10 @@ map.on("click", function (e) {
 
 // Making a marker with a custom icon
 
-  const issIcon = L.icon({
-    iconUrl: 'iss200.png',
-    iconSize: [50, 32],
-    iconAnchor: [25, 16]
-  });
-  const marker = L.marker([0, 0], { icon: issIcon }).addTo(map);
-
-  const api_url = 'https://api.wheretheiss.at/v1/satellites/25544';
-
-  let firstTime = true;
-
-  async function getISS() {
-    const response = await fetch(api_url);
-    const data = await response.json();
-    const { latitude, longitude } = data;
-
-    marker.setLatLng([latitude, longitude]);
-    if (firstTime) {
-      map.setView([latitude, longitude], 2);
-      firstTime = false;
+fetch("incidents.json")
+  .then((res) => res.json())
+  .then((incidents) => {
+    for (const incident of incidents) {
+      L.circle([incident.lat, incident.lon], { radius: 1 }).addTo(map);
     }
-    document.getElementById('lat').textContent = latitude.toFixed(2);
-    document.getElementById('lon').textContent = longitude.toFixed(2);
-  }
-
-  getISS();
-
-
-  setInterval(getISS, 1000);
-
-  fetch("incidents.json")
-    .then(res => res.json())
-    .then(incidents => {
-      for (const incident of incidents) {
-        L.circle([incident.lat, incident.lon], {radius: 1}).addTo(map)
-      }
-    })
+  });
