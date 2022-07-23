0bd2f22c488f9e64f0b86d91c4475b7000349744
revosw <simenwiik@hotmail.com>
2022-07-23 21:36:06 +0200

    Adds popup to circles

--- app.js
+++ app.js
@@ -21,40 +21,42 @@ map.on("click", function (e) {
 
 // Making a marker with a custom icon
 
-  const issIcon = L.icon({
-    iconUrl: 'iss200.png',
-    iconSize: [50, 32],
-    iconAnchor: [25, 16]
-  });
-  const marker = L.marker([0, 0], { icon: issIcon }).addTo(map);
+const issIcon = L.icon({
+  iconUrl: 'iss200.png',
+  iconSize: [50, 32],
+  iconAnchor: [25, 16]
+});
+const marker = L.marker([0, 0], { icon: issIcon }).addTo(map);
 
-  const api_url = 'https://api.wheretheiss.at/v1/satellites/25544';
+const api_url = 'https://api.wheretheiss.at/v1/satellites/25544';
 
-  let firstTime = true;
+let firstTime = true;
 
-  async function getISS() {
-    const response = await fetch(api_url);
-    const data = await response.json();
-    const { latitude, longitude } = data;
+async function getISS() {
+  const response = await fetch(api_url);
+  const data = await response.json();
+  const { latitude, longitude } = data;
 
-    marker.setLatLng([latitude, longitude]);
-    if (firstTime) {
-      map.setView([latitude, longitude], 2);
-      firstTime = false;
-    }
-    document.getElementById('lat').textContent = latitude.toFixed(2);
-    document.getElementById('lon').textContent = longitude.toFixed(2);
+  marker.setLatLng([latitude, longitude]);
+  if (firstTime) {
+    map.setView([latitude, longitude], 2);
+    firstTime = false;
   }
+  document.getElementById('lat').textContent = latitude.toFixed(2);
+  document.getElementById('lon').textContent = longitude.toFixed(2);
+}
 
-  getISS();
+getISS();
 
 
-  setInterval(getISS, 1000);
+setInterval(getISS, 1000);
 
-  fetch("incidents.json")
-    .then(res => res.json())
-    .then(incidents => {
-      for (const incident of incidents) {
-        L.circle([incident.lat, incident.lon], {radius: 1}).addTo(map)
-      }
-    })
+fetch("incidents.json")
+  .then(res => res.json())
+  .then(incidents => {
+    for (const incident of incidents) {
+      L.circle([incident.lat, incident.lon], { radius: 25 })
+        .addTo(map)
+        .bindPopup(incident.name)
+    }
+  })
