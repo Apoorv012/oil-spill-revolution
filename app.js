61ed97d5f3bd4f5b8ddb9164b2fc3aa59bd103ed
revosw <simenwiik@hotmail.com>
2022-07-23 21:01:28 +0200

    fixes json and adds incident markers

--- app.js
+++ app.js
@@ -20,31 +20,41 @@ map.on("click", function (e) {
 });
 
 // Making a marker with a custom icon
-//   const issIcon = L.icon({
-//     iconUrl: 'iss200.png',
-//     iconSize: [50, 32],
-//     iconAnchor: [25, 16]
-//   });
-//   const marker = L.marker([0, 0], { icon: issIcon }).addTo(mymap);
 
-//   const api_url = 'https://api.wheretheiss.at/v1/satellites/25544';
+  const issIcon = L.icon({
+    iconUrl: 'iss200.png',
+    iconSize: [50, 32],
+    iconAnchor: [25, 16]
+  });
+  const marker = L.marker([0, 0], { icon: issIcon }).addTo(map);
 
-//   let firstTime = true;
+  const api_url = 'https://api.wheretheiss.at/v1/satellites/25544';
 
-//   async function getISS() {
-//     const response = await fetch(api_url);
-//     const data = await response.json();
-//     const { latitude, longitude } = data;
+  let firstTime = true;
 
-//     marker.setLatLng([latitude, longitude]);
-//     if (firstTime) {
-//       mymap.setView([latitude, longitude], 2);
-//       firstTime = false;
-//     }
-//     document.getElementById('lat').textContent = latitude.toFixed(2);
-//     document.getElementById('lon').textContent = longitude.toFixed(2);
-//   }
+  async function getISS() {
+    const response = await fetch(api_url);
+    const data = await response.json();
+    const { latitude, longitude } = data;
 
-//   getISS();
+    marker.setLatLng([latitude, longitude]);
+    if (firstTime) {
+      map.setView([latitude, longitude], 2);
+      firstTime = false;
+    }
+    document.getElementById('lat').textContent = latitude.toFixed(2);
+    document.getElementById('lon').textContent = longitude.toFixed(2);
+  }
 
-//   setInterval(getISS, 1000);
+  getISS();
+
+
+  setInterval(getISS, 1000);
+
+  fetch("incidents.json")
+    .then(res => res.json())
+    .then(incidents => {
+      for (const incident of incidents) {
+        L.circle([incident.lat, incident.lon], {radius: 1}).addTo(map)
+      }
+    })
